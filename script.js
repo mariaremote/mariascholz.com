26eb7e841d744f221f280074454138d485c8de69
Maria Scholz <123465523+mariaremote@users.noreply.github.com>
2024-02-13 08:24:37 +0000

    attempt fixing hamburger menu

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const hamburger = document.querySelector('#hamburger');
-const nav = document.querySelector('.navbar');
+const hamburger = document.getElementById('hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () =>
+    navbar.style.display === 'block' ? (navbar.style.display = 'none') : (navbar.style.display = 'block')
+);
 
 // Verify that both of those are found by adding an `if` statement check that both of them are "truthy"
 // https://developer.mozilla.org/en-US/docs/Glossary/Truthy
-if (hamburger && nav) {
-    hamburger.addEventListener('click', function () {
-        if (nav.style.display === 'block') {
-            nav.style.display = 'none';
-        } else {
-            nav.style.display = 'block';
-        }
-    });
-} else {
-    // Dump an informative error to the console and handle things gracefully instead of crashing.
-    if (!hamburger) console.error('#hamburger element not found!');
-    if (!nav) console.error('.navbar element not found!');
-}
+// if (hamburger && navbar) {
+//     hamburger.addEventListener('click', () =>
+//         navbar.style.display === 'block' ? (navbar.style.display = 'none') : (navbar.style.display = 'block')
+//     );
+// } else {
+//     // Dump an informative error to the console and handle things gracefully instead of crashing.
+//     if (!hamburger) console.error('#hamburger element not found!');
+//     if (!navbar) console.error('.navbar element not found!');
+// }
 
 // document.querySelector('#hamburger').addEventListener('click', function () {
 //         const nav = document.querySelector('.navbar');
