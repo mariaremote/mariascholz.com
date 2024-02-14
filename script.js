7f70e25bd495aa6b524f14bd9720cdfe8b853612
surfineer <123465523+surfineer@users.noreply.github.com>
2024-02-14 09:34:35 +0100

    finished feature

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ if (hamburger && nav) {
 //             nav.style.display = 'block';
 //         }
 //     });
+
+// TECHSTACK SPAN
+
+const techStack = ['Python', 'Flask', 'Javascript', 'HTML', 'CSS'];
+const techStackSpan = document.getElementById('typewriter');
+const pauseBetweenWords = 1500;
+const typingInterval = 150;
+let indexTechStack = 0;
+let indexWord = 0;
+
+function typeTechStack() {
+    if (indexWord < techStack[indexTechStack].length) {
+        // Add next character
+        techStackSpan.textContent += techStack[indexTechStack].charAt(indexWord);
+        indexWord++;
+        setTimeout(typeTechStack, typingInterval);
+    } else {
+        // Pause after typing a word
+        setTimeout(() => {
+            techStackSpan.textContent = ''; // Clear the span
+            indexWord = 0; // Reset character index
+            indexTechStack = (indexTechStack + 1) % techStack.length; // when last index modulo array length is zero = reset index
+            typeTechStack(); // Start typing the next word
+        }, pauseBetweenWords);
+    }
+}
+document.addEventListener('DOMContentLoaded', typeTechStack);
